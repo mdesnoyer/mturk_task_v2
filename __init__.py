79ae6a766bbd6480e21cb17c6cd5039f433f58b1
Nick <dufour@neon-lab.com>
2016-01-27 13:58:48 -0800

    hopefully the base works omg

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# import sys
-# import os
-#
-#
-# # handle the importing of the system path that contains this directory.
-# base_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-# if base_path not in sys.path:
-#     sys.path.insert(0, base_path)
