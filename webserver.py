ad036e7a778f8bf380fa0450469154259f58dda3
Nick <dufour@neon-lab.com>
2016-03-04 12:26:58 -0800

    V

--- webserver.py
+++ webserver.py
@@ -537,4 +537,4 @@ if __name__ == '__main__':
                 debug=True, ssl_context=context)
     else:
         app.run(host='0.0.0.0', port=WEBSERVER_PORT, threaded=True,
-                debug=False)
+                debug=True)
