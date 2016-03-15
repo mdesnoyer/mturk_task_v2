cfbe1b77541d0b0ef8585890f88e296ffb1e6987
Nick <dufour@neon-lab.com>
2016-03-15 11:50:18 -0700

    created a script to write the sampling data to a file

--- conf.py
+++ conf.py
@@ -147,7 +147,7 @@ KEYWORDS = []  # the keywords for the HIT / HIT Type
 """
 PRACTICE TASK CONFIGURATION
 """
-DEF_PRACTICE_NUM_IMAGES_PER_TASK = 30  # the default number of images
+DEF_PRACTICE_NUM_IMAGES_PER_TASK = 60  # the default number of images
                                        # per practice.
 DEF_PRACTICE_INTO_INSTRUCTIONS = \
     '/instructions/practice_template.html'  # the default practice introduction
@@ -158,7 +158,7 @@ DEF_PRACTICE_PROMPT = \
 DEF_PRACTICE_KEEP_BLOCKS = 1  # the default number of keep blocks for a
                               # practice task
 DEF_PRACTICE_REJECT_BLOCKS = 1  # the default number of reject blocks for a task
-DEFAULT_PRACTICE_PAYMENT = 0.12  # the default payment for practices
+DEFAULT_PRACTICE_PAYMENT = 0.20  # the default payment for practices
 DEFAULT_PRACTICE_TASK_NAME = \
     DEFAULT_TASK_NAME + ' PRACTICE'  # The title for practice tasks.
 PRACTICE_DESCRIPTION = \
---
+++ dump_sampling_data.py
@@ -0,0 +1,33 @@
+"""
+Dumps the image sampling data to a file (/tmp/datas) as a stored numpy array.
+"""
+
+from conf import *
+from db import Get
+import happybase
+import numpy as np
+
+conn = happybase.Connection(DATABASE_LOCATION)
+pool = happybase.ConnectionPool(size=3, host=DATABASE_LOCATION)
+dbget = Get(pool)
+
+print 'Getting active images'
+active_ids = []
+samp_cnts = []
+win_cnts = []
+samp_surp = []
+t = conn.table(IMAGE_TABLE)
+
+scnr = dbget.get_active_image_scanner()
+for n in range(dbget.get_n_active_images_count()):
+    x = scnr.next()
+    active_ids.append(x)
+    samp_cnts.append(t.counter_get(x, 'stats:num_times_seen'))
+    win_cnts.append(t.counter_get(x, 'stats:num_wins'))
+    samp_surp.append(t.counter_get(x, 'stats:sampling_surplus'))
+    print n
+
+with open('/tmp/datas', 'w') as f:
+    np.save(np.array(active_ids, samp_cnts, win_cnts, samp_surp))
+
+
