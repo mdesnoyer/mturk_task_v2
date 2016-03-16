9fda7692d621caba35689b70c04685beb47b06c4
Nick <dufour@neon-lab.com>
2016-03-15 18:16:51 -0700

    here goes nothing, going live

--- reset.py
+++ reset.py
@@ -16,9 +16,9 @@ import os
 import boto
 
 reset_database = True  # whether to rebuild the databases
-reset_quals = False  # remove qualifications from MTurk
+reset_quals = True  # remove qualifications from MTurk
 reset_hits = True  # whether or not to remove all extant hits
-autopass_practice = True  # whether or not to automatically pass krypton
+autopass_practice = False  # whether or not to automatically pass krypton
 
 wid = 'A1RPGGKICRYDKC'  # the krypton worker id
 
