5ee891d1f9e261046fbfd531ec34f49a676329f5
Nick <dufour@neon-lab.com>
2016-03-28 12:41:15 -0700

    permit dumping of win table data

---
+++ dump_to_csv.py
@@ -0,0 +1,19 @@
+"""
+Takes the win data and dumps it to a CSV file in /tmp as /tmp/win_data.csv
+"""
+
+from conf import *
+import happybase
+
+conn = happybase.Connection(DATABASE_LOCATION)
+
+t = conn.table(WIN_TABLE)
+s = t.scan()
+
+with open('/tmp/data', 'w') as f:
+    for n, (k, data) in enumerate(s):
+        print n
+        winner = data['data:winner_id']
+        loser = data['data:loser_id']
+        win_cnt = t.counter_get(k, 'data:win_count')
+        f.write('%s,%s,%i\n' % winner, loser, win_cnt)
