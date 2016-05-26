947a45b33eec41201ec3f3f60e762865d3903013
Nick Dufour <dufour.nicholas@gmail.com>
2016-05-25 17:17:30 -0700

    blah

--- testing/populate_task_times.py
+++ testing/populate_task_times.py
@@ -5,25 +5,36 @@ within the completion data that includes the total time in seconds.
 At least, that was its original intent. However, it looks like the original
 get_mean_times function was extremely inefficient. so I updated it, and this
 might be sufficient on its own.
+
+ITS NOT! Not by like a massive longshot...
+
+so, back to the original plan of using the distinct column.
 """
 
 from conf import *
-from db import Get
 import happybase
-import time
+import json
 
 _log = logger.setup_logger(__name__)
 
-pool = happybase.ConnectionPool(size=2, host=DATABASE_LOCATION)
-dbget = Get(pool)
-
-print 'new-er version...'
-
-avg_meth_new = dbget._get_mean_task_time_old()
-
-print 'newest version...'
-
-avg_meth_newest = dbget._get_mean_task_time()
-
-print 'newer:',avg_meth_new,'newest:',avg_meth_newest
+conn = happybase.Connection(host=DATABASE_LOCATION)
+table = conn.table(TASK_TABLE)
+s = table.scan(columns=['completion_data:response_json',
+                        'status:accepted',
+                        'metadata:is_practice'])
+for n, (id, data) in enumerate(s):
+    print n
+    if data.get('metadata:is_practice', FALSE) == TRUE:
+        table.put(id, {'completion_data:total_time': '0'})
+        continue
+    if data.get('status:accepted', FALSE) == FALSE:
+        table.put(id, {'completion_data:total_time': '0'})
+        continue
+    jsn_str = data.get('completion_data:response_json', None)
+    if jsn_str is None:
+        table.put(id, {'completion_data:total_time': '0'})
+        continue
+    jsn = json.loads(jsn_str)
+    tot_time = float(jsn[-1]['time_elapsed']) / 1000
+    table.put(id, {'completion_data:total_time': str(int(tot_time))})
 
