4b1f00a296b5b1d312ecfb5ac689c4824fe2c85e
Nick Dufour <ndufour@eunoia.home>
2016-06-02 10:24:17 -0400

    reduced number of tasks available online

--- conf.py
+++ conf.py
@@ -89,7 +89,7 @@ MTURK OPTIONS
 """
 LOW_FUNDS_WARNING = 300  # if the funds drop below this amount
 NUM_PRACTICES = 5  # 10  # how many practices to post at once
-NUM_TASKS = 100  # how many tasks to maintain online
+NUM_TASKS = 40  # how many tasks to maintain online
 NUM_ASSIGNMENTS_PER_PRACTICE = 1      # how many people can take a given
                                       # practice?
 # HIT_LIFETIME_IN_SECONDS = 60*60*24*30  # How long a hit lasts. The current value
