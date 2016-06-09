9829a5e7dc857c109df8c22d34887aeeab43f18f
Nick Dufour <ndufour@eunoia.home>
2016-06-09 10:49:16 -0400

    tweak

--- conf.py
+++ conf.py
@@ -245,7 +245,7 @@ WEBSERVER CONFIGURATION
 """
 NUM_THREADS = 1  # the number of threads to use on the webserver.
 ENABLE_BANNING = True  # whether or not to ban poor-performing workers.
-SAMPLE_COUNT_REFRESH_RATE = 1000000  # how many samples until you rebuild
+SAMPLE_COUNT_REFRESH_RATE = 4000000  # how many samples until you rebuild
 # sampler
 SAMPLING_LIMIT = 4  # how many samples to obtain per task before stopping
 
